917246d7d1dca49e393895037653b19a24cd9792
Talha shiekh <tk.shiekh4567@gmail.com>
2024-09-19 06:54:27 -0700

    Pending Video screen

--- Screens/VideoGenerationScreen.jsx
+++ Screens/VideoGenerationScreen.jsx
@@ -1,20 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import ReusableScreenContainer from "../Components/ReusableScreenContainer";
 import HeadingAndDesc from "../Components/ToolsHeadingAndDesc";
-import ImagePromptInputAndButton from "../Components/ImagePromptInputAndButton";
-import { Player } from "video-react";
 
 const VideoGenerationScreen = () => {
-
-  const [prompt,setprompt] = useState("");
-
-  async function handleVideoGeneration(){
-
-
-  }
-
-
-
   return (
     <ReusableScreenContainer>
       <HeadingAndDesc
@@ -25,20 +13,6 @@ const VideoGenerationScreen = () => {
         }
         description="Transform your words into stunning videos with HiSkyVid"
       />
-      <ImagePromptInputAndButton
-        value={prompt}
-        setvalue={setprompt}
-        handleImageGeneration={handleVideoGeneration}
-        placeholder={
-          "Fierce warrior in futuristic armor, standing tall under a stormy sky."
-        }
-      />
-      {/* <Player
-          playsInline
-          fluid
-          autoPlay={true}
-          src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
-        /> */}
     </ReusableScreenContainer>
   );
 };
