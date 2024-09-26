8fbb7f51a4453959a635212e9ed50687d02cf5ab
Talha shiekh <tk.shiekh4567@gmail.com>
2024-09-26 06:29:43 -0700

    Pending applying video url

--- Screens/VideoGenerationScreen.jsx
+++ Screens/VideoGenerationScreen.jsx
@@ -7,29 +7,48 @@ import { Player } from "video-react";
 const VideoGenerationScreen = () => {
   const [prompt, setprompt] = useState("");
 
+  let myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
   async function handleVideoGeneration() {
-    const url =
-      "https://text-to-video.p.rapidapi.com/v3/process_text_and_search_media";
-    const options = {
+    let raw = JSON.stringify({
+      key: import.meta.env.VITE_SECRET_VIDEO_GENERATION_KEY,
+      model_id: "zeroscope",
+      prompt: "A cat riding on a bike",
+      negative_prompt: "low quality",
+      height: 320,
+      width: 500,
+      num_frames: 16,
+      num_inference_steps: 20,
+      guidance_scale: 7,
+      upscale_height: 640,
+      upscale_width: 1024,
+      upscale_strength: 0.6,
+      upscale_guidance_scale: 12,
+      upscale_num_inference_steps: 20,
+      output_type: "gif",
+      webhook: null,
+      track_id: null,
+    });
+
+    var requestOptions = {
       method: "POST",
-      headers: {
-        "x-rapidapi-key": "f8779f6b33msh87f375a21ad501ep1d6054jsne95f7029c532",
-        "x-rapidapi-host": "text-to-video.p.rapidapi.com",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        script:
-          "Welcome to our YouTube channel where we will guide you through an exciting 2-day itinerary to visit Bali. Day 1, start your morning by exploring the iconic Tanah Lot temple and enjoy the stunning sunrise views. Afterward, head to the laid-back town of Ubud, where you can visit the famous Monkey Forest and experience the traditional Balinese culture. For lunch, try the local delicacies at one of the many Warungs (small local restaurants) in Ubud. In the afternoon, make your way to Tegalalang Rice Terrace, a beautiful and breathtaking sight that will surely leave you in awe. Next, visit the Ubud Art Market and shop for unique souvenirs, handmade crafts, and beautiful artwork created by local artisans. Before dinner, take a yoga class or indulge in a relaxing Balinese massage to unwind and rejuvenate. On day 2, start your morning with a trip to the stunning Tirta Gangga water palace and enjoy serenity surrounded by beautiful water gardens. Afterward, head to the Tegenungan Waterfall and take a refreshing dip in the crystal-clear water. For lunch, try the famous Babi Guling (suckling pig) at Ibu Oka, a well-known restaurant in Ubud. In the afternoon, visit the incredible Uluwatu Temple and be amazed by its cliffside location and panoramic ocean views. Stay until sunset and witness the mesmerizing Kecak Fire Dance performance. Before your trip ends, don't forget to enjoy a seafood dinner at Jimbaran Bay, where you can feast on fresh catch while enjoying the beautiful beach atmosphere. That concludes our 2-day itinerary to visit Bali, packed with incredible sights, cultural experiences, and mouthwatering food. Make sure to subscribe to our channel for more travel tips and amazing destinations.",
-        dimension: "16:9",
-      }),
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
     };
 
     try {
-      const response = await fetch(url, options);
-      const result = await response.json();
-      console.table(result);
-    } catch (error) {
-      console.error(error);
+      const request = await fetch(
+        "https://modelslab.com/api/v6/video/text2video",
+        requestOptions
+      );
+      const response = await request.json();
+      setTimeout(() => {
+        console.log(response.future_links[0])
+      },response.eta * 1000)
+    } catch (e) {
+      console.log(e);
     }
   }
 
